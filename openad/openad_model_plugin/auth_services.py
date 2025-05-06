82e081e77d2360040960478fcf28932492c21dd7
Moenen Erbuer <moenen.erbuer@ibm.com>
2025-05-06 09:53:18 -0400

    Removed auth refresh stuff

--- openad/openad_model_plugin/auth_services.py
+++ openad/openad_model_plugin/auth_services.py
@@ -63,34 +63,9 @@ def get_service_api_key(service_name: str) -> str:
     auth_lookup_table = load_lookup_table()
     # find group name belonging to service
     auth_group = auth_lookup_table["service_table"].get(service_name, "")
-    print("\n\n", 14, service_name, auth_group)
     api_key = auth_lookup_table["auth_table"].get(auth_group, "")
     logger.debug(f"get service api key | {service_name=} {auth_group=} {api_key=}")
     return api_key
-
-
-def update_lookup_table(auth_group, api_key=None, service=None, hide_api=False) -> LookupTable:
-    """update the lookup table values on either api_key or model service"""
-    print("update_lookup_table -->", auth_group)
-    logger.debug(f"updating auth group '{auth_group}' {api_key=} {service=}")
-    # Load the existing data or create a new dictionary
-    data = load_lookup_table()
-    print("\n\n", 11, data.get("auth_table").get("gcloud"))
-    # Update the dictionary with new key-value pairs
-    if api_key:
-        # create auth group entry
-        data["auth_table"].update({auth_group: api_key})
-    if service and auth_group in data["auth_table"]:
-        # map a model service to auth group
-        data["service_table"].update({service: auth_group})
-
-    print("\n\n", 12, data.get("auth_table").get("gcloud"))
-    # Save the updated dictionary back to the pickle file
-    save_lookup_table(data)
-    # return latest data
-    if hide_api:
-        return hide_api_keys(data)
-    return data
 
 
 def remove_service_group(service_name, hide_api=False) -> LookupTable:
